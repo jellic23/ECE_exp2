0b58f11959523692c5a7eb461b84d57a4ebfd0be
jellic23 <144265303+jellic23@users.noreply.github.com>
2023-11-24 16:22:16 +0900

    교안 10주차, 11주차 결과.
    교안 10주차, 11주차에 해당하는 text LCD v2, DAC 실험 후의 코드들입니다.

--- week10/project_1/project_1.srcs/sources_1/new/LCD_cursor.v
+++ week10/project_1/project_1.srcs/sources_1/new/LCD_cursor.v
@@ -1,16 +1,21 @@
 `timescale 1ns / 1ps
 
 
-module LCD_cursor(rst, clk, LCD_E, LCD_RS, LCD_RW, LCD_DATA, LED_out, number_btn, control_btn);
+module LCD_cursor(rst, clk, LCD_E, LCD_RS, LCD_RW, LCD_DATA, LED_out, number_btn, control_btn, dip_btn);
 
 input rst, clk;
 input [9:0] number_btn;
 input [1:0] control_btn;
+input dip_btn;
 
 wire [9:0] number_btn_t;
 wire [1:0] control_btn_t;
+wire dip_btn_t;
 
-oneshot_universal #(.WIDTH(12)) One1(clk, rst, {number_btn[9:0], control_btn[1:0]}, {number_btn_t[9:0], control_btn_t[1:0]});
+integer i;
+integer x;
+
+oneshot_universal #(.WIDTH(13)) One1(clk, rst, {number_btn[9:0], control_btn[1:0],dip_btn}, {number_btn_t[9:0], control_btn_t[1:0],dip_btn_t});
 
 output LCD_E, LCD_RS, LCD_RW;
 output reg [7:0] LCD_DATA;
@@ -21,15 +26,16 @@ reg LCD_RS, LCD_RW;
 
 reg [7:0] cnt;
 
-reg [2:0] state;
-parameter DELAY = 3'b000,
-           FUNCTION_SET = 3'b001,
-           DISP_ONOFF = 3'b010,
-           ENTRY_MODE = 3'b011,
-           SET_ADDRESS = 3'b100,
-           DELAY_T = 3'b101,
-           WRITE = 3'b110,
-           CURSOR = 3'b111;
+reg [3:0] state;
+parameter DELAY = 4'b0000,
+           FUNCTION_SET = 4'b0001,
+           DISP_ONOFF = 4'b0010,
+           ENTRY_MODE = 4'b0011,
+           SET_ADDRESS = 4'b0100,
+           DELAY_T = 4'b0101,
+           WRITE = 4'b0110,
+           CURSOR = 4'b0111,
+           LINE = 4'b1000;
            
 always @(posedge clk or negedge rst)
 begin
@@ -60,7 +66,7 @@ begin
                 LED_out <= 8'b0000_1000;
             end
             DELAY_T : begin
-                state <= |number_btn_t ? WRITE : (|control_btn_t ? CURSOR : DELAY_T);
+                state <= |number_btn_t ? WRITE : (|control_btn_t ? CURSOR : (|dip_btn_t ? LINE : DELAY_T));
                 LED_out <= 8'b0000_0100;
             end
             WRITE : begin
@@ -70,6 +76,10 @@ begin
             CURSOR : begin
                 if(cnt==30) state <= DELAY_T;
                 LED_out <= 8'b0000_0001;
+            end
+            LINE : begin
+                if(cnt==30) state <= DELAY_T;
+                LED_out <= 8'b0000_0000;
             end
         endcase
     end
@@ -105,14 +115,22 @@ begin
             CURSOR : 
                 if(cnt >= 30) cnt <=0;
                 else cnt <= cnt +1;
+            LINE : 
+                if(cnt >= 30) cnt <=0;
+                else cnt <= cnt +1;
+                
         endcase
     end
 end
 
 always @(posedge clk or negedge rst)
 begin
-    if(!rst)
+    if(!rst) begin
+    
         {LCD_RS, LCD_RW, LCD_DATA} <= 10'b0_0_0000_0001;
+        i<=0;
+        x<=0;
+    end
     else
     begin
         case(state)
@@ -140,18 +158,48 @@ begin
                         10'b0000_0000_10 : {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0011_1001; //9
                         10'b0000_0000_01 : {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0011_0000; //0
                     endcase
+                    x=x+1;
                 end
                 else {LCD_RS, LCD_RW, LCD_DATA} <= 10'b0_0_0000_1111;
             end
             CURSOR : begin
                 if(cnt == 20) begin
                     case(control_btn)
-                        2'b10 : {LCD_RS, LCD_RW, LCD_DATA} <= 10'b0_0_0001_0000; //left
-                        2'b01 : {LCD_RS, LCD_RW, LCD_DATA} <= 10'b0_0_0001_0100; //right
+                        2'b10 : begin
+                            {LCD_RS, LCD_RW, LCD_DATA} <= 10'b0_0_0001_0000;
+                            x=x-1;
+                            if(x==16) {LCD_RS, LCD_RW, LCD_DATA} = 10'b0_0_1000_0000;
+                        end //left
+                        2'b01 : begin
+                            {LCD_RS, LCD_RW, LCD_DATA} <= 10'b0_0_0001_0100;
+                            x=x+1;
+                            if(x==61) {LCD_RS, LCD_RW, LCD_DATA} = 10'b0_0_1100_0000; //right
+                        end
                     endcase
+                    
                 end
                 else {LCD_RS, LCD_RW, LCD_DATA} <= 10'b0_0_0000_1111;
+                
+        /*        if(cnt==21 && x==16) {LCD_RS, LCD_RW, LCD_DATA} = 10'b0_0_1000_0000;
+                if(cnt==21 && x==61) {LCD_RS, LCD_RW, LCD_DATA} = 10'b0_0_1100_0000; */
+                
+                
             end
+            LINE : begin
+                if(cnt == 20) begin
+                    if(dip_btn==1)
+                         if(i==0) begin
+                            {LCD_RS, LCD_RW, LCD_DATA} = 10'b0_0_1100_0000;
+                            i<=i+1;
+                            x=0;
+                         end
+                         else if(i==1) begin
+                            {LCD_RS, LCD_RW, LCD_DATA} = 10'b0_0_1000_0000;
+                             i<=0;
+                             x=40;
+                         end
+                    end
+                end
         endcase
     end
 end
